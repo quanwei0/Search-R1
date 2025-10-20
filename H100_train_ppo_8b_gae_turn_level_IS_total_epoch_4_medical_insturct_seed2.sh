c9ebadf495d7f22628b66a2eb78e41f1198ea192
Chenliang Li <chenliang@example.com>
2025-10-20 19:35:38 +0000

    update bash

--- H100_train_ppo_8b_gae_turn_level_IS_total_epoch_4_medical_insturct_seed2.sh
+++ H100_train_ppo_8b_gae_turn_level_IS_total_epoch_4_medical_insturct_seed2.sh
@@ -16,7 +16,7 @@ WAND_PROJECT='Search-R1'
 
 
 export BASE_MODEL='meta-llama/Meta-Llama-3-8B-Instruct'
-export EXPERIMENT_NAME=Medical-H100-search-r1-ppo-meta-llama-3-8b-it-em-gae-turn-IS-total-epoch-4-Minibatch256-saved-checkpoint-seed3-lr-warmup-600-valid-search-penalty-0.1-final-answer-penalty-0.3
+export EXPERIMENT_NAME=Medical-H100-search-r1-ppo-meta-llama-3-8b-it-em-gae-turn-IS-total-epoch-4-Minibatch256-saved-checkpoint-seed3-lr-warmup-600-valid-search-penalty-0.1-final-answer-penalty-0.3-no-varnacne-reduction
 # export BASE_MODEL='Qwen/Qwen2.5-1.5B-Instruct'
 # export EXPERIMENT_NAME=nq-search-r1-ppo-qwen2.5-1.5b-it-em
 # export BASE_MODEL='Qwen/Qwen2.5-3B'
@@ -70,7 +70,6 @@ PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.temperature=1 \
     actor_rollout_ref.actor.state_masking=True \
     +actor_rollout_ref.actor.importance_sampling_level=token \
-    +actor_rollout_ref.actor.detach_ratio=variance_reduction \
     critic.optim.lr=1e-5 \
     critic.model.use_remove_padding=True \
     critic.optim.lr_warmup_steps_ratio=0.015 \
