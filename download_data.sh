fa91717e1fd677b531d9c2873ebcb49b7aa51e0b
quanwei0 <weiquan0128@gmail.com>
2026-01-10 00:58:51 -0600

    add bash

---
+++ download_data.sh
@@ -0,0 +1,11 @@
+source activate searchr1
+
+pip install verl==0.1.0
+save_path=./data
+python scripts/download.py --save_path $save_path
+cat $save_path/part_* > $save_path/e5_Flat.index
+gzip -d $save_path/wiki-18.jsonl.gz
+
+# python scripts/data_process/nq_search.py
+huggingface-cli download --repo-type dataset --local-dir ./data/nq_hotpotqa_train  PeterJinGo/nq_hotpotqa_train
+pip uninstall verl -y
